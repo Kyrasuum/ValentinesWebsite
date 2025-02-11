1e33d3c6144b82de1738cbdef692f4c7ad4ab04d
phillip lorentz <plorentz7@gmail.com>
2025-02-11 09:17:35 -0600

    changing to only one page

--- source/src/router/router.ts
+++ source/src/router/router.ts
@@ -2,21 +2,9 @@ import { RouteRecordRaw } from 'vue-router';
 
 const routes: RouteRecordRaw[] = [
     {
-        path: import.meta.env.VITE_ASSET_URL + '/valentines/:catchAll(.*)*',
+        path: '/:catchAll(.*)*',
         name: "valentines",
         component: () => import('/src/pages/valentines.vue'),
-        meta: {
-            requiresAuth: false,
-            level: 0
-        }
-    },
-
-    // Always leave this as last one,
-    // but you can also remove it
-    {
-        path: '/:catchAll(.*)*',
-        name: "redirect",
-        component: () => import('/src/pages/redirect.vue'),
         meta: {
             requiresAuth: false,
             level: 0
